d412c20745c09c6a50cd21c161b68691268345cc
LynnT <LynnTCai@gmail.com>
2024-01-30 13:35:12 -0600

    lab 3

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Create your views here.
@@ -17,3 +18,19 @@ def dogindex(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', { 'dog': dog })
+
+class DogList(ListView):
+    model = Dog
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/{id}'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = '__all__'
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogindex'
