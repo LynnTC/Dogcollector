da2e404142f01fc599b6d3ad8f1835c144da0bb0
LynnT <LynnTCai@gmail.com>
2024-01-31 17:00:37 -0600

    lab 5

--- main_app/models.py
+++ main_app/models.py
@@ -7,11 +7,22 @@ MEALS = (
     ('D', 'Dinner'),
 )
 
+class Toy(models.Model):
+    name= models.CharField(max_length=100)
+    color= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+    
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
