b85981cc3701b173eb65151ea0d3c779eb88a9d3
LynnT <LynnTCai@gmail.com>
2024-01-31 16:04:15 -0600

    lab 4

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('dogindex/', views.dogindex, name='index'),
     path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
+    path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name="add_feeding"),
     path('dogs/', views.DogList.as_view(), name='dogs_index'),
     path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
     path('dogs/<int:pk>/update', views.DogUpdate.as_view(), name='dogs_update'),
